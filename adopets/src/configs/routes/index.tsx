63c95d3219dbe39ef30d0fd553092df01a242e54
fabioNog <fabio.carmo@computacao.ufla.br>
2019-11-26 19:18:49 -0300

    modifield component Routes, alter for typescript and add PrivateRoute and add auth auth.tsx for text of the authenticated

--- adopets/src/configs/routes/index.tsx
+++ adopets/src/configs/routes/index.tsx
@@ -1,19 +1,30 @@
  import React from "react";
  import Provider from 'redux'
- import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+ import { BrowserRouter, Route, Switch,Redirect } from 'react-router-dom'
  
 import Login from '../../components/Login'
+import {isAutheticated} from './auth'
 
+
+const PrivateRoute = ({component, isAuthenticated, ...rest}: any) => {
+    const routeComponent = (props: any) => (
+        isAuthenticated
+            ? React.createElement(component, props)
+            : <Redirect to={{pathname: '/'}}/>
+    );
+    return <Route {...rest} render={routeComponent}/>;
+};
  
  function AppRouter() {
    return (
-   <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Login} />
-          </Switch>
-        </div>
-      </Router>
+   <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/app" component={() => <h1>Você está logado</h1>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
    );
  }
 
