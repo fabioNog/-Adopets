273e042bc0fdae92878205e1f98c0c6be9fa24ff
fabioNog <fabio.carmo@computacao.ufla.br>
2019-11-27 00:02:20 -0300

    testing and add props isAuthenticated in component privateRoutes

--- adopets/src/configs/routes/index.tsx
+++ adopets/src/configs/routes/index.tsx
@@ -1,4 +1,4 @@
- import React from "react";
+ import React , {useState} from "react";
  import Provider from 'redux'
  import { BrowserRouter, Route, Switch,Redirect } from 'react-router-dom'
  
@@ -6,22 +6,25 @@ import Login from '../../components/Login'
 import {isAuthenticated} from './auth'
 
 
-const PrivateRoute = ({component, isAuthenticated, ...rest}: any) => {
-    const routeComponent = (props: any) => (
-        isAuthenticated
-            ? React.createElement(component, props)
-            : <Redirect to={{pathname: '/login'}}/>
-    );
-    return <Route {...rest} render={routeComponent}/>;
-};
+
  
  function AppRouter() {
+   const [auth,setAuth] = useState(true)
+   
+   const PrivateRoute = ({component, isAuthenticated, ...rest}: any) => {
+      const routeComponent = (props: any) => (
+          isAuthenticated
+              ? React.createElement(component, props)
+              : <Redirect to={{pathname: '/login'}}/>
+      );
+      return <Route {...rest} render={routeComponent}/>;
+    };
    return (
    <BrowserRouter>
       <div>
         <Switch>
           <Route exact path="/login" component={Login} />
-          <PrivateRoute path="/" component={() => <h1>Você está logado</h1>} />
+          <PrivateRoute isAuthenticated={auth} path="/" component={() => <h1>Você está logado</h1>} />
         </Switch>
       </div>
     </BrowserRouter>
